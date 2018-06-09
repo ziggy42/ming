57c2a28156839c720eb86694fc318cdee2c4d864
AndreaPivetta <vanpivix@gmail.com>
2018-06-09 16:16:37 +0200

    Simplify api gateway response building

--- src/main/kotlin/com/serverless/ApiGatewayResponse.kt
+++ src/main/kotlin/com/serverless/ApiGatewayResponse.kt
@@ -1,42 +1,14 @@
 package com.serverless
 
-import com.fasterxml.jackson.core.JsonProcessingException
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import org.apache.logging.log4j.LogManager
-import org.apache.logging.log4j.Logger
-import java.util.Collections
 
-class ApiGatewayResponse(
-    val statusCode: Int = 200,
-    var body: String? = null,
-    val headers: Map<String, String>? = Collections.emptyMap()
-) {
+private val mapper = jacksonObjectMapper()
 
-    companion object {
-        inline fun build(block: Builder.() -> Unit) = Builder().apply(block).build()
-    }
+fun <T> buildApiGatewayResponse(
+    body: T,
+    statusCode: Int = 200,
+    headers: Map<String, String>? = mapOf()
+): ApiGatewayResponse = ApiGatewayResponse(statusCode, mapper.writeValueAsString(body), headers)
 
-    class Builder {
-        var LOG: Logger = LogManager.getLogger(ApiGatewayResponse.Builder::class.java)
-        var objectMapper = jacksonObjectMapper()
-
-        var statusCode: Int = 200
-        var headers: Map<String, String>? = Collections.emptyMap()
-        var objectBody: Response? = null
-
-        fun build(): ApiGatewayResponse {
-            var body: String? = null
-
-            if (objectBody != null) {
-                try {
-                    body = objectMapper.writeValueAsString(objectBody)
-                } catch (e: JsonProcessingException) {
-                    LOG.error("failed to serialize object", e)
-                    throw RuntimeException(e)
-                }
-            }
-
-            return ApiGatewayResponse(statusCode, body, headers)
-        }
-    }
-}
+@Suppress("unused")
+class ApiGatewayResponse(val statusCode: Int, val body: String?, val headers: Map<String, String>?)
--- src/main/kotlin/com/serverless/Handler.kt
+++ src/main/kotlin/com/serverless/Handler.kt
@@ -4,19 +4,18 @@ import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
 import org.apache.logging.log4j.LogManager
 
+private val logger = LogManager.getLogger(Handler::class.java)
+
 class Handler : RequestHandler<Map<String, Any>, ApiGatewayResponse> {
 
     override fun handleRequest(input: Map<String, Any>, context: Context): ApiGatewayResponse {
-        LOG.info("received: " + input.keys.toString())
+        logger.info("received: " + input.keys.toString())
 
-        return ApiGatewayResponse.build {
-            statusCode = 200
-            objectBody = HelloResponse("Go Serverless v1.x! Your Kotlin function executed successfully!", input)
-            headers = mapOf("X-Powered-By" to "AWS Lambda & serverless")
-        }
-    }
-
-    companion object {
-        private val LOG = LogManager.getLogger(Handler::class.java)
+        return buildApiGatewayResponse(
+            HelloResponse(
+                "Go Serverless v1.x! Your Kotlin function executed successfully!",
+                input
+            )
+        )
     }
 }
