34d4a007bea4f924ed34ca92ee7e67a4779cfe83
AndreaPivetta <vanpivix@gmail.com>
2018-06-09 15:58:12 +0200

    Remove useless response fields

--- src/main/kotlin/com/serverless/ApiGatewayResponse.kt
+++ src/main/kotlin/com/serverless/ApiGatewayResponse.kt
@@ -4,15 +4,12 @@ import com.fasterxml.jackson.core.JsonProcessingException
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
-import java.nio.charset.StandardCharsets
-import java.util.Base64
 import java.util.Collections
 
 class ApiGatewayResponse(
     val statusCode: Int = 200,
     var body: String? = null,
-    val headers: Map<String, String>? = Collections.emptyMap(),
-    val isBase64Encoded: Boolean = false
+    val headers: Map<String, String>? = Collections.emptyMap()
 ) {
 
     companion object {
@@ -24,28 +21,22 @@ class ApiGatewayResponse(
         var objectMapper: ObjectMapper = ObjectMapper()
 
         var statusCode: Int = 200
-        var rawBody: String? = null
         var headers: Map<String, String>? = Collections.emptyMap()
         var objectBody: Response? = null
-        var binaryBody: ByteArray? = null
-        var base64Encoded: Boolean = false
 
         fun build(): ApiGatewayResponse {
             var body: String? = null
 
-            if (rawBody != null) {
-                body = rawBody as String
-            } else if (objectBody != null) {
+            if (objectBody != null) {
                 try {
                     body = objectMapper.writeValueAsString(objectBody)
                 } catch (e: JsonProcessingException) {
                     LOG.error("failed to serialize object", e)
                     throw RuntimeException(e)
                 }
-            } else if (binaryBody != null) {
-                body = String(Base64.getEncoder().encode(binaryBody), StandardCharsets.UTF_8)
             }
-            return ApiGatewayResponse(statusCode, body, headers, base64Encoded)
+
+            return ApiGatewayResponse(statusCode, body, headers)
         }
     }
 }
